8487f04ebd1b83b560e69f4d9d0f6c7c4774de26
InSeop Hwang <inseop.hwang@hotmail.com>
2024-03-03 23:20:07 +0900

    to add function push(array add)

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const a = 2
 const b = 1
+const dayOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"]
 
 console.log(a + b)
 console.log(a * b)
 console.log(a / b)
+console.log(dayOfWeek)
+dayOfWeek.push("sun")
+
+console.log(dayOfWeek)
 
