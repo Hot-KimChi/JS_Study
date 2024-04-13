99ad98bae57aa034f83f7b9e832db2b65c2b3d9d
InSeop Hwang <inseop.hwang@hotmail.com>
2024-04-13 23:40:31 +0900

    let, const and var

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 // variables 만드는 2가지 방법
-let a = 2;
+// - [let] 은 뒤쪽에서 update가 가능
+// - [const] 는 constant 라는 의미로 뒤쪽에서 No-update
+const a = 2;
 const b = 1;
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 
-const myName = "nico";
+let myName = "nico";
 console.log("hello " + myName);
+
+myName = "nicolas"
+console.log("your new name is " + myName);
 
 
 const dayOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
